3dbfc946b23d22535cea0fc5c1e7593606af96f9
unknown <jaj9608@mavs.uta.edu>
2023-06-21 16:24:41 -0500

    q10 q11 q12? q13a

--- app.py
+++ app.py
@@ -51,92 +51,150 @@ def favicon():
 @app.route('/count', methods=['POST'])
 def count():
     # SQL query to retrieve earthquakes of a specific type and magnitude greater than a threshold
-    query = '''SELECT *
-    FROM cities
-    WHERE idx = {}
-    '''
+     # Retrieving data from the request form
     result = None
     arg = request.form.get('name')
-    if not arg.isnumeric():
-        return redirect(url_for('index'))
-    arg = int(arg)
+    args = arg.split(',')
+    args[0] = int(args[0])
+    args[1] = int(args[1])
+    query = '''SELECT *
+    FROM cities1
+    WHERE Population BETWEEN {} AND {}
+    '''
+    result = None
     rands = []
     times = []
     tot = 0
-    for i in range(arg):
-        rands.append(random.randint(0,10000))
-    for i in rands:
-        start = time.time()
-        result = pd.read_sql_query(query.format(i), engine)
-        finish = time.time()
-        times.append(str(i)+": "+str(finish-start))
-        tot += (finish-start)
+    start = time.time()
+    result = pd.read_sql_query(query.format(args[0],args[1]), engine)
+    finish = time.time()
+    times.append(str(finish-start))
+    tot += (finish-start)
     # Rendering the template with the query result
     
     if result is not None:
-       return render_template('count.html',total = "Total Time: "+str(tot), name = times)
+       return render_template('count.html',total = "Total Time: "+str(tot), tables=[result.to_html(classes='data', header="true")])
     else:
        print('Request for count page received with no name or blank name -- redirecting')
        
 @app.route('/reg_small', methods=['POST'])
 def reg_small():
     # SQL query to retrieve earthquakes of a specific type and magnitude greater than a threshold
-    query = '''SELECT *
-    FROM cities
-    WHERE idx = {}
-    '''
     result = None
     arg = request.form.get('name')
-    if not arg.isnumeric():
-        return redirect(url_for('index'))
-    arg = int(arg)
+    args = arg.split(',')
+    args[0] = int(args[0])
+    args[1] = int(args[1])
+    args[2] = int(args[2])
+    query = '''SELECT *
+    FROM cities1
+    WHERE Population BETWEEN {} AND {}
+    '''
+    result = None
     rands = []
     times = []
     tot = 0
-    for i in range(arg):
-        rands.append(random.randint(0,10))
+    result = pd.read_sql_query(query.format(args[0],args[1]), engine)
+    idxs = []
+    res = None
+    query = '''SELECT *
+    FROM cities1
+    WHERE idx = {}
+    '''
+    for r in result['idx']:
+        idxs.append(r) 
+    for i in range(args[2]):
+        rands.append(random.randint(min(idxs),max(idxs)))
     for i in rands:
         start = time.time()
-        result = pd.read_sql_query(query.format(i), engine)
+        if res is None:
+            res = pd.read_sql_query(query.format(i), engine)
+        else:
+            r = pd.read_sql_query(query.format(i), engine)
+            res = pd.concat([res,r], ignore_index=True)
         finish = time.time()
-        times.append(str(i)+": "+str(finish-start))
         tot += (finish-start)
+        times.append(str(finish-start))
     # Rendering the template with the query result
+    result = res
     
     if result is not None:
-       return render_template('count.html',total = "Total Time: "+str(tot), name = times)
+       return render_template('count.html',total = "Total Time: "+str(tot), times = times, tables=[result.to_html(classes='data', header="true")])
     else:
        print('Request for count page received with no name or blank name -- redirecting')
+
+@app.route('/increment', methods=['POST'])
+def increment():
+    # Retrieving data from the request form
+    result = None
+    arg = request.form.get('name')
+    args = arg.split(',')
+    args[1] = int(args[1])
+    args[2] = int(args[2])
+    args[3] = int(args[3])
+    query = '''SELECT *
+    FROM cities
+    WHERE State = {}
+    AND Population BETWEEN {} AND {}
+    '''
+    
+    # Table name, field to increment, and population range
+    metadata = MetaData(bind=engine)
+    table_name = 'cities'
+    table = Table(table_name, metadata, autoload=True)
+    field_to_increment = 'Population'
+    min_population = args[1]
+    max_population = args[2]
+
+    
+    # Construct the update query
+    stmt = (
+        update(table)
+        .where(table.c.Population.between(args[0],min_population, max_population))
+        .values({field_to_increment: table.c.Population + args[3]})  
+    )
+    start = time.time()
+
+    # Execute the update query
+    with engine.begin() as connection:
+        connection.execute(stmt)
+    finish = time.time()
+    time = finish-start
+    result = pd.read_sql_query(query.format(args[0],min_population,max_population), engine)
+    
+    # Rendering the template with the query result
+    if result is not None:
+       return render_template('count.html', total = time, tables=[result.to_html(classes='data', header="true")])
+    else:
+       print('Request for count page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
 @app.route('/cache_all', methods=['POST'])
 def cache_all():
     # SQL query to retrieve earthquakes of a specific type and magnitude greater than a threshold
-    query = '''SELECT *
-    FROM cities
-    WHERE idx = {}
-    '''
     result = None
     arg = request.form.get('name')
-    if not arg.isnumeric():
-        return redirect(url_for('index'))
-    arg = int(arg)
+    args = arg.split(',')
+    args[0] = int(args[0])
+    args[1] = int(args[1])
+    query = '''SELECT *
+    FROM cities1
+    WHERE Population BETWEEN {} AND {}
+    '''
     rands = []
     times = []
     tot = 0
-    for i in range(arg):
-        rands.append(random.randint(0,10000))
-    for i in rands:
-        start = time.time()
-        result = pa.default_serialization_context()
-        res = r.get(i)
-        if res is not None:
-            result.deserialize(res)
-        else:
-            result = pd.read_sql_query(query.format(i), engine)
-            r.set(i,pa.default_serialization_context().serialize(result).to_buffer().to_pybytes())
-        finish = time.time()
-        times.append(str(i)+": "+str(finish-start))
-        tot += (finish-start)
+    start = time.time()
+    result = pa.default_serialization_context()
+    res = r.get(arg)
+    if res is not None:
+        result.deserialize(res)
+    else:
+        result = pd.read_sql_query(query.format(args[0],args[1]), engine)
+        r.set(arg,pa.default_serialization_context().serialize(result).to_buffer().to_pybytes())
+    finish = time.time()
+    times.append(str(arg)+": "+str(finish-start))
+    tot += (finish-start)
     # Rendering the template with the query result
     
     if result is not None:
